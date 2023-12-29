eb5d33da5fefb5b910e52112d3ae7803d5c74da3
Sirshak Bohara <boharasirshak101@gmail.com>
2023-12-29 23:55:10 +0545

    Restructured project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Client, LocalAuth, Message } from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
+import express, {Request, Response} from 'express';
 
 const client = new Client({
     authStrategy: new LocalAuth({
         dataPath: '/session'
     })
 });
+const app = express();
+
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -24,3 +27,13 @@ client.on('message', async (message: Message) => {
 });
 
 client.initialize();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server started!');
+})
+
