83abf7a16037ee174620407e288eda2bcca09f68
Sirshak Bohara <boharasirshak101@gmail.com>
2023-12-30 17:18:13 +0545

    Add message acknowledgment handling and validation middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, {Request, Response} from 'express';
-import { Chat } from 'whatsapp-web.js';
+import { Message, MessageAck } from 'whatsapp-web.js';
 import client from './client';
 import qrcode from 'qrcode-terminal';
 import clientReadyMiddleware from './middleware/clientReadyMiddleware';
+import numberVerifyMiddleware from './middleware/numberVerifyMiddleware';
 
 const app = express();
 app.use(express.json());
@@ -39,6 +40,32 @@ client.on('authentication_failure', () => {
     console.log('Auth failure!');
 });
 
+client.on('message_ack', (message: Message, ack: MessageAck) => {
+    let sentTo = message.to;
+    let messageId = message.id;
+    if (ack === MessageAck.ACK_ERROR){
+        console.log(`Message ${messageId} sent to ${sentTo} failed to send`);
+    }
+    else if (ack === MessageAck.ACK_PENDING){
+        console.log(`Message ${messageId} sent to ${sentTo} is pending to be seen`);
+    }
+    else if (ack === MessageAck.ACK_SERVER){
+        console.log(`Message ${messageId} sent to ${sentTo} was sent by the server`);
+    }
+    else if (ack === MessageAck.ACK_DEVICE){
+        console.log(`Message ${messageId} sent to ${sentTo} was received on the device`);
+    }
+    else if (ack === MessageAck.ACK_READ){
+        console.log(`Message ${messageId} sent to ${sentTo} was read by the recipient`);
+    }
+    else if (ack === MessageAck.ACK_PLAYED){
+        console.log(`Message ${messageId} sent to ${sentTo} was played by the recipient`);
+    }
+    else {
+        console.log(`Message ${messageId} sent to ${sentTo} has an unknown ack`);
+    }
+});
+
 
 app.get('/', async (req: Request, res: Response) => {
     res.send('Hello World!');
@@ -60,32 +87,82 @@ app.get('/me', clientReadyMiddleware, async (req: Request, res: Response) => {
 });
 
 
-app.post('/is_valid', clientReadyMiddleware, async (req: Request, res: Response) => {
-    if (!req.body.number){
-        return res.status(400).send({
-            error: 'Missing number on request body'
-        });
-    }
-    var number: string = req.body.number as string;
-
-    if (!(/^\+[0-9]*?$/.test(number))) {
-        return res.status(400).send({
-            error: 'Invalid number format. Use E.164 format, i.e. [+][country-code][10-digit-number-without-dashes]'
-        });
-    } 
-
-    // remove the + from the number
-    number = number.replace(/\+/g, '');
-
-    // add @c.us if it's not present because whatsapp ID must be in this format to be checked by the whatsapp-web.js library
-    if (!number.endsWith('@c.us')) {
-        number += '@c.us';
-    }
-    
-    let isValid = await client.isRegisteredUser(number);
-    res.status(200).send({
+app.post('/is_valid', clientReadyMiddleware, numberVerifyMiddleware, async (req: Request, res: Response) => {
+    let isValid = await client.isRegisteredUser(req.body.number as string);
+    let statusCode = isValid ? 200 : 400;
+    res.status(statusCode).send({
         is_valid: isValid
     });
+});
+
+
+app.post('/send_message', clientReadyMiddleware, numberVerifyMiddleware, async (req: Request, res: Response) => {
+    if (!req.body.message){
+        return res.status(400).send({
+            error: 'Missing message on request body'
+        });
+    }
+
+    var number: string = req.body.number as string;
+    var message: string = req.body.message as string;
+
+    if (message.length === 0) {
+        return res.status(400).send({
+            error: 'Message is empty'
+        });
+    }
+    if (message.length > 4096) {
+        return res.status(400).send({
+            error: 'Message is too long. Max length is 4096 characters'
+        });
+    }
+
+    let isValid = await client.isRegisteredUser(req.body.number as string);
+    if (!isValid) {
+        return res.status(400).send({
+            error: 'Number is not registered on Whatsapp'
+        });
+    }
+
+    let sentMessage = await client.sendMessage(number, message);
+    let messageId: object = sentMessage.id;
+    let sentTo: string = sentMessage.to;
+
+    var messageStatus = '';
+    switch (sentMessage.ack) {
+        case MessageAck.ACK_ERROR:
+            messageStatus = 'Failed to send';
+            break;
+        case MessageAck.ACK_PENDING:
+            messageStatus = 'Pending to be seen';
+            break;
+        case MessageAck.ACK_SERVER:
+            messageStatus = 'Server';
+            break;
+        case MessageAck.ACK_DEVICE:
+            messageStatus = 'Device';
+            break;
+        case MessageAck.ACK_READ:
+            messageStatus = 'Read by recipient';
+            break;
+        case MessageAck.ACK_PLAYED:
+            messageStatus = 'Played by recipient';
+            break;
+    }
+
+    if (sentMessage.ack == MessageAck.ACK_ERROR) {
+        return res.status(400).send({
+            error: 'Message failed to send'
+        });
+    }
+
+    res.status(200).send({
+        status: messageStatus,
+        sent_to: sentTo,
+        message_id: messageId,
+    });
+
+
 });
 
 app.listen(3000, () => {
