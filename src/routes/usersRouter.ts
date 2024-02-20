1fa51d46989de4347ad338e895c335febf7fdf3c
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-20 21:38:12 +0545

    refactor: formatted

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express'
+import multer from 'multer'
+import * as os from 'os'
 import { getMe, sendMessageToUser, verifyNumber } from '../controllers/usersControllers'
 import isClientReady from '../middleware/clientMiddlewares'
 import messageFormatVerify from '../middleware/messageMiddlewares'
 import numberFormater from '../middleware/numberMiddlewares'
 
+const upload = multer({
+  storage: multer.diskStorage({ destination: os.tmpdir() }),
+  limits: { fileSize: 2048 * 1024 * 1024 }
+})
+
 const router = Router()
 
 router.get('/me', isClientReady, getMe)
 router.post('/verify', isClientReady, numberFormater, verifyNumber)
-router.post('/sendMessage', isClientReady, numberFormater, messageFormatVerify, sendMessageToUser)
+router.post(
+  '/sendMessage',
+  isClientReady,
+  numberFormater,
+  messageFormatVerify,
+  upload.any(),
+  sendMessageToUser
+)
 
 export default router
