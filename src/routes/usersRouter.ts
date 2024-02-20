2aba623160ecf011ea7facff165035ae0b76fc2e
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-20 18:06:48 +0545

    refactor: rename

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { getMe } from '../controllers/usersControllers'
-import isClientReady from '../middleware/clientReadyMiddleware'
+import isClientReady from '../middleware/clientMiddlewares'
 
 const router = Router()
 
