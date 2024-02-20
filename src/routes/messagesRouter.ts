e45705c3ed327c55718d55c9c1f561838ee7ed3c
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-20 18:32:54 +0545

    refactor: removed the /verify endpoint to /users route

--- src/routes/messagesRouter.ts
+++ src/routes/messagesRouter.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express'
 import { sendMessageToUser } from '../controllers/messagesControllers'
-import { verifyNumber } from '../controllers/usersControllers'
 import isClientReady from '../middleware/clientMiddlewares'
 import numberFormater from '../middleware/numberMiddlewares'
 
 const router = Router()
 
-router.post('/verify', isClientReady, numberFormater, verifyNumber)
 router.post('/send', isClientReady, numberFormater, sendMessageToUser)
 
 export default router
--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { getMe } from '../controllers/usersControllers'
+import { getMe, verifyNumber } from '../controllers/usersControllers'
 import isClientReady from '../middleware/clientMiddlewares'
+import numberFormater from '../middleware/numberMiddlewares'
 
 const router = Router()
 
 router.get('/me', isClientReady, getMe)
+router.post('/verify', isClientReady, numberFormater, verifyNumber)
 
 export default router
