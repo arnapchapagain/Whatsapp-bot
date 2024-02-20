bbfcd3a43e2ec2c2e0dcee9c75dc3b1acac8418e
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-20 18:05:52 +0545

    feat: added all the client events and handlers to the respective file

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,15 @@
-import { Client, LocalAuth, type Message } from 'whatsapp-web.js'
+import { exit } from 'process'
+import qrcode from 'qrcode-terminal'
+import { Client, LocalAuth, MessageAck, type Message } from 'whatsapp-web.js'
 import JsonDb from './utils/db'
 
-const db = new JsonDb<Message>('/session/messages.json')
+const db = new JsonDb<Message>('./session/messages.json')
+
+declare global {
+  var IS_READY: boolean
+  var IS_AUTHENTICATED: boolean
+  var client: Client
+}
 
 const client = new Client({
   authStrategy: new LocalAuth({
@@ -9,13 +17,56 @@ const client = new Client({
   })
 })
 
+globalThis.IS_READY = false
+globalThis.IS_AUTHENTICATED = false
+globalThis.client = client
+
 client.on('message', (message) => {
-  console.log(message.body)
+  console.log('New Message received', message)
   db.insertOne(message)
   db.commit()
 
   if (message.body === '!ping') {
     message.reply('pong').catch(err => { console.error(err) })
+  }
+})
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true })
+})
+
+client.on('ready', () => {
+  console.log('Client is ready!')
+  globalThis.IS_READY = true
+})
+
+client.on('authenticated', () => {
+  globalThis.IS_AUTHENTICATED = true
+  console.log('Client is authenticated!')
+})
+
+client.on('authentication_failure', () => {
+  console.log('Auth failure!')
+  exit(1)
+})
+
+client.on('message_ack', (message: Message, ack: MessageAck) => {
+  const sentTo = '+' + message.to.replace('@c.us', '')
+  const messageId = message.id.id
+  if (ack === MessageAck.ACK_ERROR) {
+    console.log(`Message ${messageId} sent to ${sentTo} failed to send`)
+  } else if (ack === MessageAck.ACK_PENDING) {
+    console.log(`Message ${messageId} sent to ${sentTo} is pending to be seen`)
+  } else if (ack === MessageAck.ACK_SERVER) {
+    console.log(`Message ${messageId} sent to ${sentTo} was sent by the server`)
+  } else if (ack === MessageAck.ACK_DEVICE) {
+    console.log(`Message ${messageId} sent to ${sentTo} was received on the device`)
+  } else if (ack === MessageAck.ACK_READ) {
+    console.log(`Message ${messageId} sent to ${sentTo} was read by the recipient`)
+  } else if (ack === MessageAck.ACK_PLAYED) {
+    console.log(`Message ${messageId} sent to ${sentTo} was played by the recipient`)
+  } else {
+    console.log(`Message ${messageId} sent to ${sentTo} has an unknown ack`)
   }
 })
 
