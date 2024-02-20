156555381a3d636b25eb4736fabd2a34740d0709
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-20 19:03:46 +0545

    feat!: relocated the send message to user function to the /users/sendMessage route

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,13 +1,18 @@
 import type { Request, Response } from 'express'
+import { MessageAck } from 'whatsapp-web.js'
 
-export function getMe (req: Request, res: Response): void {
+export async function getMe (req: Request, res: Response): Promise<void> {
   const me = globalThis.client.info.wid
   const name = globalThis.client.info.pushname
   const platform = globalThis.client.info.platform
+  const chats = await globalThis.client.getChats()
+  const groups = chats.filter(chat => chat.isGroup)
   res.status(200).send({
     ...me,
     name,
-    platform
+    platform,
+    chats,
+    groups
   })
 }
 
@@ -16,5 +21,57 @@ export async function verifyNumber (req: Request, res: Response): Promise<void> 
   const statusCode = isValid ? 200 : 400
   res.status(statusCode).send({
     is_valid: isValid
+  })
+}
+
+export async function sendMessageToUser (req: Request, res: Response): Promise<void> {
+  const message = req.body.message as string ?? ''
+  const number: string = req.body.number as string ?? ''
+
+  const isValid = await client.isRegisteredUser(req.body.number as string)
+  if (!isValid) {
+    res.status(400).send({
+      error: 'Number is not registered on Whatsapp'
+    })
+    return
+  }
+
+  const sentMessage = await client.sendMessage(number, message)
+  const messageId: object = sentMessage.id
+  const sentTo: string = sentMessage.to
+
+  let messageStatus = ''
+  switch (sentMessage.ack) {
+    case MessageAck.ACK_ERROR:
+      messageStatus = 'Failed to send'
+      break
+    case MessageAck.ACK_PENDING:
+      messageStatus = 'Pending to be seen'
+      break
+    case MessageAck.ACK_SERVER:
+      messageStatus = 'Server'
+      break
+    case MessageAck.ACK_DEVICE:
+      messageStatus = 'Device'
+      break
+    case MessageAck.ACK_READ:
+      messageStatus = 'Read by recipient'
+      break
+    case MessageAck.ACK_PLAYED:
+      messageStatus = 'Played by recipient'
+      break
+  }
+
+  if (sentMessage.ack === MessageAck.ACK_ERROR) {
+    res.status(400).send({
+      error: 'Message failed to send'
+    })
+    return
+  }
+
+  res.status(200).send({
+    status: messageStatus,
+    sent_to: sentTo,
+    message_id: messageId
   })
 }
--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
-import { getMe, verifyNumber } from '../controllers/usersControllers'
+import { getMe, sendMessageToUser, verifyNumber } from '../controllers/usersControllers'
 import isClientReady from '../middleware/clientMiddlewares'
+import messageFormatVerify from '../middleware/messageMiddlewares'
 import numberFormater from '../middleware/numberMiddlewares'
 
 const router = Router()
 
 router.get('/me', isClientReady, getMe)
 router.post('/verify', isClientReady, numberFormater, verifyNumber)
+router.post('/sendMessage', isClientReady, numberFormater, messageFormatVerify, sendMessageToUser)
 
 export default router
