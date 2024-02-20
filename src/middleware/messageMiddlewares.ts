691304edd45182a9cdc527f6936a41918e7652f5
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-20 21:37:48 +0545

    feat: removed the emoty message handler

--- src/middleware/messageMiddlewares.ts
+++ src/middleware/messageMiddlewares.ts
@@ -10,19 +10,6 @@ import type { NextFunction, Request, Response } from 'express'
 export default function messageFormatVerify (req: Request, res: Response, next: NextFunction): void {
   const message = req.body.message as string ?? ''
 
-  if (message === '') {
-    res.status(400).send({
-      error: 'Missing message on request body'
-    })
-    return
-  }
-
-  if (message.length === 0) {
-    res.status(400).send({
-      error: 'Message is empty'
-    })
-    return
-  }
   if (message.length > 4096) {
     res.status(400).send({
       error: 'Message is too long. Max length is 4096 characters'
