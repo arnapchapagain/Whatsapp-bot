ec596b7b430e575621c26b0300f1be87b9ab09b5
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-21 15:47:39 +0545

    feat: added send files option to groups

--- src/v1/controllers/groupsControllers.ts
+++ src/v1/controllers/groupsControllers.ts
@@ -1,54 +1,69 @@
 import type { Request, Response } from 'express'
-import { MessageAck } from 'whatsapp-web.js'
+import { MessageAck, MessageMedia, type Message } from 'whatsapp-web.js'
+import { formatMessageAck } from '../../utils/formatter'
 
+// TODO: This method is copy-pasted from the sendMessageToUser method in usersControllers.ts
+// Need to create a seperate sendMessage function and use it in both usersControllers.ts and groupsControllers.ts
 export async function sendMessageToGroup (req: Request, res: Response): Promise<void> {
   const message = req.body.message as string ?? ''
   const groupId: string = req.body.group_id as string ?? ''
 
   try {
     const group = await globalThis.client.getChatById(groupId)
-    const sentMessage = await group.sendMessage(message)
-    const messageId: object = sentMessage.id
-    const sentTo: string = sentMessage.to
+    const files = req.files as Express.Multer.File[] ?? []
+    const length = req.files?.length as number ?? 0
 
-    let messageStatus = ''
-    switch (sentMessage.ack) {
-      case MessageAck.ACK_ERROR:
-        messageStatus = 'Failed to send'
-        break
-      case MessageAck.ACK_PENDING:
-        messageStatus = 'Pending to be seen'
-        break
-      case MessageAck.ACK_SERVER:
-        messageStatus = 'Server'
-        break
-      case MessageAck.ACK_DEVICE:
-        messageStatus = 'Device'
-        break
-      case MessageAck.ACK_READ:
-        messageStatus = 'Read by recipient'
-        break
-      case MessageAck.ACK_PLAYED:
-        messageStatus = 'Played by recipient'
-        break
-    }
+    if (length === 0) {
+      const sentMessage = await group.sendMessage(message)
+      const messageStatus = formatMessageAck(sentMessage.ack)
 
-    if (sentMessage.ack === MessageAck.ACK_ERROR) {
-      res.status(400).send({
-        error: 'Message failed to send'
+      if (sentMessage.ack === MessageAck.ACK_ERROR) {
+        res.status(400).send({
+          error: 'Message failed to send'
+        })
+        return
+      }
+
+      res.status(200).send({
+        data: {
+          status: messageStatus,
+          ...sentMessage
+        }
       })
       return
     }
 
+    const sendAsDocument = Boolean(req.body.send_as_document) ?? false
+    const responses: Message[] = []
+
+    for (let i = 0; i < length; i++) {
+      const file = MessageMedia.fromFilePath(files[i].path)
+      file.mimetype = files[i].mimetype
+      file.filename = files[i].originalname
+      const sentMessage = await group.sendMessage(
+        file,
+        {
+          caption: message,
+          sendMediaAsDocument: sendAsDocument
+        }
+      )
+
+      if (sentMessage.ack === MessageAck.ACK_ERROR) {
+        res.status(400).send({
+          error: 'Message failed to send'
+        })
+        return
+      }
+      responses.push(sentMessage)
+    }
     res.status(200).send({
-      status: messageStatus,
-      sent_to: sentTo,
-      message_id: messageId
+      data: responses
     })
-  } catch {
-    res.status(404).send({
+  } catch (e) {
+    console.log(e)
+    res.status(500).send({
       error: {
-        message: 'Group not found'
+        message: 'Failed to send message'
       }
     })
   }
--- src/v1/routes/groupsRouter.ts
+++ src/v1/routes/groupsRouter.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express'
+import multer from 'multer'
+import * as os from 'os'
+
 import { sendMessageToGroup } from '../controllers/groupsControllers'
 import isClientReady from '../middleware/clientMiddlewares'
 import messageFormatVerify from '../middleware/messageMiddlewares'
 
 const router = Router()
 
-router.post('/sendMessage', isClientReady, messageFormatVerify, sendMessageToGroup)
+const upload = multer({
+  storage: multer.diskStorage({ destination: os.tmpdir() }),
+  limits: { fileSize: 2048 * 1024 * 1024 }
+})
+
+router.post(
+  '/sendMessage',
+  isClientReady,
+  upload.any(),
+  messageFormatVerify,
+  sendMessageToGroup
+)
 
 export default router
